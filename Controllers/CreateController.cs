0095108dabd826adb42c6bdb4b0cdd5e1811e109
matveygutor <matweyhutorts@gmail.com>
2023-09-30 01:41:10 +0300

    Реализован функционал генерации токена

--- Controllers/CreateController.cs
+++ Controllers/CreateController.cs
@@ -8,16 +8,19 @@ using Microsoft.EntityFrameworkCore;
 using test;
 using test.Models;
 using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Text;
 
 namespace test.Controllers
 {
     public class CreateController : Controller
     {
         private readonly LinkContext _context;
-
+        private Random _random;
         public CreateController(LinkContext context)
         {
             _context = context;
+            _random = new Random();
         }
 
         // GET: Create
@@ -47,7 +50,9 @@ namespace test.Controllers
         // GET: Create/Create
         public IActionResult Create()
         {
+
             return View();
+
         }
 
 
@@ -58,14 +63,29 @@ namespace test.Controllers
         {
             if (ModelState.IsValid)
             {
-                link.ShortURL = "localhost:7080/";
+                link.ShortURL = "https://" + Request.Host + "/" + GenerateToken();
                 link.Date = DateTime.Now;
                 link.Click = 0;
                 _context.Add(link);
                 await _context.SaveChangesAsync();
+                
                 return RedirectToAction(nameof(HomeController.Index));
+                
             }
+            await Response.SendFileAsync("_Layout.cshtml");
             return View(link);
+
+        }
+
+        private string GenerateToken()
+        {
+            string chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            var sb = new StringBuilder();
+            for (int i = 0; i < 7; i++)
+            {
+                sb.Append(chars[_random.Next(0, chars.Length)]);
+            }
+            return sb.ToString();
         }
 
     }
